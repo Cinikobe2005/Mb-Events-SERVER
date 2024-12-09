3b8841367a768a8700271fcbf724ba42afc7ed1b
Cinikobe2005 <adedayoabdulqadri@gmail.com>
2024-12-09 06:27:23 -0800

    fixed bug

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -15,9 +15,9 @@ const auth = require("../middleware/auth");
 router.post("/", auth, createEvents);
 router.get("/upcoming", getUpcomingEvents);
 router.get("/free", getFreeEvents);
-router.get("/", getAllEvents),
-  router.get("/hosted", auth, getHostedEvents),
-  router.post("/pay/:eventId", auth, payForAnEvent);
+router.get("/", getAllEvents);
+router.get("/hosted", auth, getHostedEvents);
+router.post("/pay/:eventId", auth, payForAnEvent);
 router.get("/previous", auth, getpreviousEvents);
 router.get("/attending", auth, getEventsToAttend);
 router.get("/:eventId", getSingleEvent);
