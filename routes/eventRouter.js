a8c5dd6064facb4f7c71480aca6755724609b052
Cinikobe2005 <adedayoabdulqadri@gmail.com>
2024-12-09 03:03:35 -0800

    added other api

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -3,13 +3,23 @@ const {
   createEvents,
   getUpcomingEvents,
   getFreeEvents,
-  getSingleEvent
+  getSingleEvent,
+  getAllEvents,
+  getEventsToAttend,
+  getpreviousEvents,
+  payForAnEvent,
+  getHostedEvents,
 } = require("../controllers/eventController");
 const auth = require("../middleware/auth");
 
 router.post("/", auth, createEvents);
 router.get("/upcoming", getUpcomingEvents);
 router.get("/free", getFreeEvents);
+router.get("/", getAllEvents),
+  router.get("/hosted", auth, getHostedEvents),
+  router.post("/pay/:eventId", auth, payForAnEvent);
+router.get("/previous", auth, getpreviousEvents);
+router.get("/attending", auth, getEventsToAttend);
 router.get("/:eventId", getSingleEvent);
 
 module.exports = router;
